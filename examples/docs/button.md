4ee6e5e8fe368df07295ff70392a9b45c7738415
stop2stare <emitremmus0235@163.com>
2017-09-05 00:17:51 +0800

    简单的button组件

--- examples/docs/button.md
+++ examples/docs/button.md
@@ -7,5 +7,7 @@
 按钮的基本用法
 
 ``` html
-<be-button></be-button>
+<be-button>基本款</be-button>
+<be-button type="info">深色款</be-button>
+<be-button type="text">简约款</be-button>
 ```
--- src/components/button/index.vue
+++ src/components/button/index.vue
@@ -1,9 +1,66 @@
 <template>
-  <button>按钮</button>
+  <button class="be-button"
+    :class="
+    [`be-button--${type}`]
+    ">
+    <span class="be-button_text"><slot></slot></span>
+  </button>
 </template>
 <script>
 export default {
-  componentName: 'be-button'
+  componentName: 'be-button',
+  props: {
+    type: {
+      type: String,
+      default: 'primary',
+      // primary: 白底 | info: 蓝底 | text: 灰底
+      validator: val => ['primary', 'info', 'text'].indexOf(val) > -1
+    }
+  }
 }
 
 </script>
+<style lang="less"
+  scoped>
+.be-button {
+  position: relative;
+  display: inline-block;
+  height: 32px;
+  line-height: 32px;
+  padding: 0 16px;
+  border-radius: 16px;
+  cursor: pointer;
+  font-size: 0;
+  outline: none;
+  border: none;
+  white-space: nowrap;
+  transition: background-color .2s ease, color .2s ease;
+  &&--text {
+    color: #333;
+    background-color: #fff;
+    &:hover {
+      background-color: darken(#fff, 5%);
+    }
+  }
+  &&--info {
+    color: #fff;
+    background-color: #37c8f7;
+    &:hover {
+      background-color: darken(#37c8f7, 5%);
+    }
+  }
+  &&--primary {
+    color: #6d757a;
+    background-color: #f1f3f7;
+    &:hover {
+      background-color: darken(#f1f3f7, 5%);
+    }
+  }
+  &_text {
+    display: inline-block;
+    font-size: 12px;
+    vertical-align: middle;
+  }
+}
+
+</style>
