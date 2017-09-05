4328f9ce3a03c337d65fee6e9da051239af79f3b
stop2stare <emitremmus0235@163.com>
2017-09-06 00:31:48 +0800

    锚链接转换成拼音

--- build/markedRenderer.js
+++ build/markedRenderer.js
@@ -1,10 +1,15 @@
 var hljs = require('highlight.js')
 var marked = require('marked')
+var pinyin = require('node-pinyin')
 var renderer = new marked.Renderer()
 var comp = ''
 
 renderer.heading = function(text, level) {
   var headerClass = ''
+  var py = pinyin(text, {
+    style: 'normal'
+  }).join('-')
+
   if (level === 2) {
     comp = text
     headerClass = 'comp-title'
@@ -12,10 +17,10 @@ renderer.heading = function(text, level) {
     headerClass = 'comp-block-title'
   }
 
-  return '<h' + level + ' id="' + text + '" class="' + headerClass + '">' +
+  return '<h' + level + ' name="' + py + '" id="' + py + '" class="' + headerClass + '">' +
     text +
     '<a class="comp-block-anchor" href="' +
-    '/#/components/' + comp + '/#' + text +
+    '/#/components/' + comp + '/#' + py +
     '">#</a>' +
     '</h' + level + '>'
 }
